ede99726a993b6f3c54cab2586317d28be19c6fa
Md Azad <81636077+mdazad2@users.noreply.github.com>
2022-09-07 07:59:59 +0530

    init

---
+++ src/script.js
@@ -0,0 +1,11 @@
+<script>
+      // code for hamburger functionality
+      function showHide() {
+        var x = document.getElementById("hamburger-menu");
+        if (x.style.display === "flex") {
+          x.style.display = "none";
+        } else {
+          x.style.display = "flex";
+        }
+      }
+</script>
